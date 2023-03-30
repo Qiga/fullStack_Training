2bfe7a89d8dfb16f820e52a8d7602c55a28ce03d
Qiga <qgam12123@gmail.com>
2023-03-30 19:29:06 +0900

    2023.03.30 19:30

--- javaFestival/src/javaFestival_2/Ex02.java
+++ javaFestival/src/javaFestival_2/Ex02.java
@@ -1,10 +1,43 @@
 package javaFestival_2;
 
+import java.util.Scanner;
+
 public class Ex02 {
 	public static void main(String[] args) {
 		
 //		정수 N을 입력받아 N * N 배열에
 //		다음과 같이 숫자를 저장하고 출력하시오. (이중 배열)
 		
+//		1 6 11 16 21
+//		2 7 12 17 22
+//		3 8 13 18 23
+//		4 9 14 19 24
+//		5 10 15 20 25
+		
+		
+		//n값 입력하기
+		Scanner sc = new Scanner(System.in);
+		System.out.print("n값 입력 : ");
+		int n = sc.nextInt();
+		
+		int[][] array = new int[n][n];
+		
+		//값 부여
+		for (int i=0; i<array.length; i++) {
+			for (int e=0; e<array[i].length; e++) {
+				array[i][e] = (i+1)+(e*n);
+			}
+		}
+		//출력결과 확인해보기
+		for (int i=0; i<array.length; i++) {
+			for (int e=0; e<array[i].length; e++) {
+				System.out.print(array[i][e] + " ");
+			}
+			System.out.println("");
+		}
+		
+		sc.close();
+		
 	}
+
 }
--- javaFestival/src/javaFestival_2/Ex06.java
+++ javaFestival/src/javaFestival_2/Ex06.java
@@ -1,10 +1,38 @@
 package javaFestival_2;
 
+
+import java.util.Random;
+
+
 public class Ex06 {
 
 	public static void main(String[] args) {
 		// 중복이 없이 숫자를 뽑는 로또 프로그램을 만드시오
+		
+		Random rd = new Random();
 
+		int[] arr = new int[6];
+		
+		//6자리 수 랜덤으로 부여하기
+		do {
+			for(int i = 0; i<arr.length; i++) {
+				arr[i] = rd.nextInt(45)+1;
+			}
+		}while(equa(arr)); //메서드로 중복확인하기 (중복일시 t/ 중복이 없으면 f)
+
+		//출력하기
+		for(int i = 0; i<arr.length; i++) {
+			System.out.println("행운의 숫자는 >> "+arr[i]);
+		}
+	
 	}
-
+	public static boolean equa(int[] a) {
+		for(int i = 0; i<a.length; i++) {
+			for(int e=i+1; e<a.length; e++) {
+				if (a[i]==a[e]) {
+					return true;					
+				}else;
+			}
+		}return false;
+	}
 }
